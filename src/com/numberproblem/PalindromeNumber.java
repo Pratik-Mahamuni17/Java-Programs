a851cf1dd4e0db4cae4b768229be28d06e939b75
Pratik Mahamuni <160023552+Pratik-Programmer@users.noreply.github.com>
2025-07-13 22:31:07 +0530

    Palindrome Number

--- src/com/numberproblem/PalindromeNumber.java
+++ src/com/numberproblem/PalindromeNumber.java
@@ -1,5 +1,28 @@
 package com.numberproblem;
 
-public class PalindromeNumber {
+import java.util.Scanner;
 
+public class PalindromeNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number: ");
+		int num = sc.nextInt();
+		
+		int org = num;
+		int rev = 0;
+		
+		while(num > 0) {
+			int digit = num % 10;
+			rev = rev * 10 + digit;
+			num /= 10;
+		}
+		
+		if(org == rev) {
+			System.out.println(org + " is a palindrome number.");
+		} else {
+			System.out.println(org + " is not a palindrome number.");
+		}
+		
+		sc.close();
+	}
 }
