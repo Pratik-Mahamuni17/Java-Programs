013be72090494652e5bf1aff88bd94d1f560ae36
Pratik Mahamuni <160023552+Pratik-Mahamuni17@users.noreply.github.com>
2025-08-15 19:32:48 +0530

    Add files via upload

---
+++ src/com/numberproblem/factorial.java
@@ -0,0 +1,14 @@
+package com.abstraction;
+
+public class factorial {
+	public static void main(String[] args) {
+		int num = 12;
+	int factorial = 0;
+	for(int i = 1;i < num;i++) {
+		factorial *= i;
+	}
+	System.out.println(factorial);
+	}
+	
+
+}
